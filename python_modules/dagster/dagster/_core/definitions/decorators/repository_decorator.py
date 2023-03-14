79b55f43aab028252d2340c320452bdc5ebca50f
benpankow <ben@elementl.com>
2023-03-14 10:19:09 -0700

    strip out nested resource anonymous identifier generation etc

--- python_modules/dagster/dagster/_core/definitions/decorators/repository_decorator.py
+++ python_modules/dagster/dagster/_core/definitions/decorators/repository_decorator.py
@@ -55,7 +55,6 @@ class _Repository:
         default_executor_def: Optional[ExecutorDefinition] = None,
         default_logger_defs: Optional[Mapping[str, LoggerDefinition]] = None,
         top_level_resources: Optional[Mapping[str, ResourceDefinition]] = None,
-        ui_visible_resources: Optional[Mapping[str, ResourceDefinition]] = None,
         resource_key_mapping: Optional[Mapping[int, str]] = None,
     ):
         self.name = check.opt_str_param(name, "name")
@@ -68,12 +67,6 @@ class _Repository:
         )
         self.top_level_resources = check.opt_mapping_param(
             top_level_resources, "top_level_resources", key_type=str, value_type=ResourceDefinition
-        )
-        self.ui_visible_resources = check.opt_mapping_param(
-            ui_visible_resources,
-            "ui_visible_resources",
-            key_type=str,
-            value_type=ResourceDefinition,
         )
         self.resource_key_mapping = check.opt_mapping_param(
             resource_key_mapping, "resource_key_mapping", key_type=int, value_type=str
@@ -142,7 +135,6 @@ class _Repository:
                     default_executor_def=self.default_executor_def,
                     default_logger_defs=self.default_logger_defs,
                     top_level_resources=self.top_level_resources,
-                    ui_visible_resources=self.ui_visible_resources,
                     resource_key_mapping=self.resource_key_mapping,
                 )
             )
@@ -205,7 +197,6 @@ def repository(
     default_executor_def: Optional[ExecutorDefinition] = ...,
     default_logger_defs: Optional[Mapping[str, LoggerDefinition]] = ...,
     _top_level_resources: Optional[Mapping[str, ResourceDefinition]] = ...,
-    _ui_visible_resources: Optional[Mapping[str, ResourceDefinition]] = ...,
     _resource_key_mapping: Optional[Mapping[int, str]] = ...,
 ) -> _Repository:
     ...
@@ -219,7 +210,6 @@ def repository(
     default_executor_def: Optional[ExecutorDefinition] = None,
     default_logger_defs: Optional[Mapping[str, LoggerDefinition]] = None,
     _top_level_resources: Optional[Mapping[str, ResourceDefinition]] = None,
-    _ui_visible_resources: Optional[Mapping[str, ResourceDefinition]] = None,
     _resource_key_mapping: Optional[Mapping[int, str]] = None,
 ) -> Union[RepositoryDefinition, PendingRepositoryDefinition, _Repository]:
     """Create a repository from the decorated function.
@@ -358,6 +348,5 @@ def repository(
         default_executor_def=default_executor_def,
         default_logger_defs=default_logger_defs,
         top_level_resources=_top_level_resources,
-        ui_visible_resources=_ui_visible_resources,
         resource_key_mapping=_resource_key_mapping,
     )
